398f9ef7d57d1840be1759ff7cccf9896b0b7a57
angmon11 <angmonscethe@gmail.com>
2026-05-29 11:09:34 +0200

    lab11

--- svg/shop/src/test/java/PlaylistTest.java
+++ svg/shop/src/test/java/PlaylistTest.java
@@ -33,4 +33,18 @@ public class PlaylistTest {
         assertTrue(playlist.contains(samesong));
         assertEquals(playlist.get(0),samesong);
     }
+    @Test
+    public void testAtSecond(){
+        Playlist playlist = new Playlist();
+        Song song1 = new Song("John Doe","Test1",100);
+        Song song2 = new Song("Mary Sue ","Test2",150);
+        Song song3 = new Song("Marty Sue","Test3",200);
+        playlist.add(song1);//0-100
+        playlist.add(song2);//100-250
+        playlist.add(song3);//250-450
+        assertEquals(song1 ,playlist.atSecond(0));
+        assertEquals(song1 ,playlist.atSecond(50));
+        assertEquals(song2 ,playlist.atSecond(200));
+        assertEquals(song3 ,playlist.atSecond(300));
+    }
 }
